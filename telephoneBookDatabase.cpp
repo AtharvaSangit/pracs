972e75b00a3789c9b0d668b491adfe743760e80e
AtharvaSangit <86745197+AtharvaSangit@users.noreply.github.com>
2022-05-27 15:14:10 +0530

    Update telephoneBookDatabase.cpp

--- telephoneBookDatabase.cpp
+++ telephoneBookDatabase.cpp
@@ -12,99 +12,91 @@ Title= Consider telephone book database of N clients. Make use of a hash table i
 5. STOP
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 class HashTable
 {
- public:
-  int index;
-   long int mobile;
+public:
+    int index;
+    long int mobile;
 };
 class Hashing
 {
     HashTable h[10];
-    public:
+
+public:
     Hashing()
     {
-        for(int i=0;i<10;i++)
+        for (int i = 0; i < 10; i++)
         {
-            h[i].index=i;
-            h[i].mobile=-1;
+            h[i].index = i;
+            h[i].mobile = -1;
         }
     }
     void display();
-    void insert(int);
+    void insert();
     void search();
     int LinearProbing(int);
-    int QuadraticProbing(int);
 };
-
-int Hashing::QuadraticProbing(int collision_position)
-{
-    
-}
 
 int Hashing::LinearProbing(int collision_position)
 {
-    for(int i=collision_position;i<10;i++)
+    for (int i = collision_position; i < 10; i++)
     {
-        if(h[i].mobile==-1)
-        return i;
-        
-        if(i==9)
+        if (h[i].mobile == -1)
+            return i;
+
+        if (i == 9)
             i = -1;
     }
 }
+
 void Hashing::display()
 {
-    for(int i=0;i<10;i++)
+    for (int i = 0; i < 10; i++)
     {
-        cout<<h[i].index;
-        cout<<"  "<<h[i].mobile;
-        cout<<endl;
+        cout << h[i].index;
+        cout << "  " << h[i].mobile;
+        cout << endl;
     }
 }
-void Hashing::insert(int probchoice)
-{   
+void Hashing::insert()
+{
     long int key;
     int position;
-    cout<<"\nEnter mobile number to insert in to hash table : ";
-    cin>>key;
+    cout << "\nEnter mobile number to insert in to hash table : ";
+    cin >> key;
     position = key % 10;
-    
-    cout<<"\nPosition = "<<position;
-    
-    if(h[position].mobile==-1)
+
+    cout << "\nPosition = " << position;
+
+    if (h[position].mobile == -1)
     {
-        h[position].mobile=key;
-    }
-    else if(probchoice ==1)// Linear Probing collision.
-    {
-        int temp_position;
-        temp_position = LinearProbing(position);
-        h[temp_position].mobile=key;
-    }
-    else if(probchoice ==2)// Quadratic Probing for collision.
-    {
-        
-    }
-    
-}
-void Hashing::search()
-{   
-    long int key;
-    int position;
-    cout<<"\nEnter mobile number to search in the hash table : ";
-    cin>>key;
-    position = key % 10;
-    
-    if(h[position].mobile==key)
-    {
-        cout<<"\nGiven mobile number is found in the hash table ";
+        h[position].mobile = key;
     }
     else
     {
-        cout<<"\nGiven mobile number is NOT found in the hash table ";
+        int temp_position;
+        temp_position = LinearProbing(position);
+        h[temp_position].mobile = key;
+    }
+}
+
+void Hashing::search()
+{
+    long int key;
+    int position;
+    cout << "\nEnter mobile number to search in the hash table : ";
+    cin >> key;
+    position = key % 10;
+
+    if (h[position].mobile == key)
+    {
+        cout << "\nGiven mobile number is found in the hash table ";
+    }
+    else
+    {
+        cout << "\nGiven mobile number is NOT found in the hash table ";
     }
 }
 int main()
@@ -113,35 +105,32 @@ int main()
     int ch;
     do
     {
-        cout<<"\n Menu";
-        cout<<"\n 1. insert";
-        cout<<"\n 2.display";
-        cout<<"\n 3.search";
-        cout<<"\n 4. exit";
-        cout<<"\n Enter your choice : ";
-        cin>>ch;
-        switch(ch)
+        cout << "\n Menu";
+        cout << "\n 1. insert";
+        cout << "\n 2.display";
+        cout << "\n 3.search";
+        cout << "\n 4. exit";
+        cout << "\n Enter your choice : ";
+        cin >> ch;
+        switch (ch)
         {
-            case 1 : //insert
-                int probchoice;
-                cout<<"\nEnter \n1 for LinearProbing \n2 for Quadratic probing";
-                cin>>probchoice;
-                H.insert(probchoice);
+        case 1: // insert
+            H.insert();
             break;
-            case 2 : //display
-                H.display();
+        case 2: // display
+            H.display();
             break;
-            
-            case 3 : //display
-                H.search();
+
+        case 3: // display
+            H.search();
             break;
-            
-            default:
-                cout<<"\nWrong choice :";
+
+        default:
+            cout << "\nWrong choice :";
             break;
         }
-        
-    }while(ch != 5);
+
+    } while (ch != 5);
     H.display();
 }
 /*Output=
@@ -151,15 +140,11 @@ int main()
  3.search
  4. exit
  Enter your choice : 1
-Enter 
-1 for LinearProbing 
-2 for Quadratic probing1
+
 Enter mobile number to insert in to hash table : 678
 Position = 8
   Enter your choice : 1
-Enter 
-1 for LinearProbing 
-2 for Quadratic probing2
+
 Enter mobile number to insert in to hash table : 546
 Position = 6
  Enter your choice : 2
@@ -175,9 +160,6 @@ Position = 6
 9  -1
 Enter your choice : 3
 Enter mobile number to search in the hash table : 678
-Given mobile number is found in the hash table 
+Given mobile number is found in the hash table
 Enter your choice : 978
 Wrong choice :*/
- 
- 
-
